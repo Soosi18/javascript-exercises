30aa30fe527f1a36cab98437765e93b43145f62a
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-03-04 22:31:46 +0530

    finish ex-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let tempInC = (tempInF - 32) * (5/9);
+  if (Number.isInteger(tempInC)){
+    return tempInC;
+  }
+  return parseFloat(tempInC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let tempInF = tempInC * (9/5) + 32
+  if (Number.isInteger(tempInF)){
+    return tempInF;
+  }
+  return parseFloat(tempInF.toFixed(1));
 };
 
 // Do not edit below this line
