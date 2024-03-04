43cb4e38bc342f23bacaaac06274a6a2d9f329dc
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-03-04 22:31:09 +0530

    finish ex-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    result = "";
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    for (let i = 0; i < num; i++) {
+        result = result.concat("", str);
+    }
+    return result;
 };
 
 // Do not edit below this line
